6fbfbc8d5ced8213fd6910116db5425575a7cde0
Roydano <roy.dano90@gmail.com>
2021-06-16 16:05:09 +0200

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // *Snack 1 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 
-let bici = [
+const bici = [
 
     {
 
@@ -58,3 +58,58 @@ console.log(
 
 
 //* Snack2 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. (utilizzate le arrow function). Infine usando la destrutturazione creiamo un nuovo array i cui elementi sono sempre degli oggetti che contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+
+
+
+const squadreCalcio = [
+
+    {
+        nome: 'Milan',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    
+    {
+        nome: 'I Gobbi',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Inter',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Atalanta',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Roma',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+
+];
+
+
+const randomNum = (min, max) => Math.floor(Math.random() * max - min + 1) + min;
+
+for(let i = 0; i < squadreCalcio.length; i++){
+    squadreCalcio[i].puntiFatti = randomNum(0,90);
+    squadreCalcio[i].falliSubiti = randomNum(0,30);
+}
+
+console.log(squadreCalcio);
+
+const squadreRidotto = [];
+
+for(let i = 0; i < squadreCalcio.length; i++){
+    const {nome, falliSubiti} = squadreCalcio[i];
+    squadreRidotto.push({nome,falliSubiti});
+}
+
+console.log(squadreRidotto);
